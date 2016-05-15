8de1dfd83cc88a51f6904091ab949f60c8ff9194
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-05-15 15:15:55 -0500

    Oops

--- src/main/java/tk/knownunown/ChatClientProtector/ChatClientProtector.java
+++ src/main/java/tk/knownunown/ChatClientProtector/ChatClientProtector.java
@@ -5,7 +5,6 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
