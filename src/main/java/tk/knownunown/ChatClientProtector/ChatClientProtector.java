2eacca79891ce3f5ebf3e2e402d1cf1955091f74
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-05-15 11:11:36 -0500

    Initial commit

---
+++ src/main/java/tk/knownunown/ChatClientProtector/ChatClientProtector.java
@@ -0,0 +1,15 @@
+package tk.knownunown.ChatClientProtector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatClientProtector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
